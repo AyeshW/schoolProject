6e96f8eaab4fd55a67e3c367f596d3bf74e13722
Hasitha Athukorala <hasithaisuru@gmail.com>
2018-04-03 22:32:50 +0530

    Hasitha changed nac bar and added login/sign-up

--- app.js
+++ app.js
@@ -3,12 +3,10 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 var login = require('./routes/login');
-
 var app = express();
 
 // view engine setup
@@ -18,8 +16,6 @@ app.set('view engine', 'jade');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -44,5 +40,13 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
 
 module.exports = app;
--- routes/login.js
+++ routes/login.js
@@ -6,16 +6,35 @@
  */
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
+var path = require('path');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post('/form',function (req,res){
+    //employee details from add-employee.html page
+    var name=req.body;
+    console.log(name);
+    res.status(200).send('employee ' + name + ' added');
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('login', { title: 'login' });
 });
 
+login("student1");
+var app = express();
+
+router.post("/login", function(req, res){
+    console.log(req.body.username);
+    res.render('/',{title:'Home'});
+});
 
 function login(username) {
-    var mysql = require('mysql');
-
     var con = mysql.createConnection({
         host: "sql12.freemysqlhosting.net",
         user: "sql12230102",
