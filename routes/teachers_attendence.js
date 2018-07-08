6c0ecb191c96c57ff575e6e2d5dd0e2a3c90bd6b
Pramith <pramith.16@cse.mrt.ac.lk>
2018-07-08 21:46:13 +0530

    Pramith added several Forms

--- routes/teachers_attendence.js
+++ routes/teachers_attendence.js
@@ -1,11 +1,40 @@
 
 var express = require('express');
 var router = express.Router();
+var mysql=require('mysql');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+    // console.log("render");
+    var con = mysql.createConnection({
+        host: "sql12.freemysqlhosting.net",
+        user: "sql12246368",
+        password: "CeVtni3mrY",
+        database: "sql12246368"
+    });
+
+    con.connect(function(err) {
+        if (err) throw err;
+        console.log("coming.....")
+        con.query("SELECT name,id FROM TeacherProfile", function (err, result, fields) {
+            if (err) throw err;
+
+            //console.log(result);
+            var string=JSON.stringify(result);
+            var jsonArray=JSON.parse(string);
+         //  var length=jsonArray.length;
+
+            res.render('teachers_attendence', { title: 'Teachers Attendence sheet',jsonArray:jsonArray });
+
+            //7 console.log(studentArray);
+
+        });
+    });
+
+
+
     res.render('teachers_attendence', { title: 'Teachers attendence '});
-    var name = 'hello';
 
 
 });
